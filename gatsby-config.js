f4c920ceef9da62f92a66884f8f4efa97f41a5ff
Justin Haupt <jhaupt100@gmail.com>
2019-04-16 22:33:19 -0700

    change favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
+    title: "Justin Haupt | Software Developer",
     author: "Hunter Chang",
     description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
   },
@@ -15,7 +15,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,100 +1,35 @@
 import React from 'react';
 import Layout from '../components/layout';
 import Header from '../components/Header';
-// import Main from '../components/Main';
 import Footer from '../components/Footer';
 
 class IndexPage extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      isArticleVisible: false,
-      timeout: false,
-      articleTimeout: false,
-      article: '',
       loading: 'is-loading'
     };
-    /*this.handleOpenArticle = this.handleOpenArticle.bind(this);
-    this.handleCloseArticle = this.handleCloseArticle.bind(this);
-    this.handleClickOutside = this.handleClickOutside.bind(this);
-    this.setWrapperRef = this.setWrapperRef.bind(this);*/
   }
 
   componentDidMount () {
     this.timeoutId = setTimeout(() => {
         this.setState({loading: ''});
     }, 100);
-    // document.addEventListener('mousedown', this.handleClickOutside);
   }
 
   componentWillUnmount () {
     if (this.timeoutId) {
         clearTimeout(this.timeoutId);
     }
-    // document.removeEventListener('mousedown', this.handleClickOutside);
   }
-
-  /*setWrapperRef(node) {
-    this.wrapperRef = node;
-  }
-
-  handleOpenArticle(article) {
-
-    this.setState({
-      isArticleVisible: !this.state.isArticleVisible,
-      article
-    })
-
-    setTimeout(() => {
-      this.setState({
-        timeout: !this.state.timeout
-      })
-    }, 325)
-
-    setTimeout(() => {
-      this.setState({
-        articleTimeout: !this.state.articleTimeout
-      })
-    }, 350)
-
-  }
-
-  handleCloseArticle() {
-
-    this.setState({
-      articleTimeout: !this.state.articleTimeout
-    })
-
-    setTimeout(() => {
-      this.setState({
-        timeout: !this.state.timeout
-      })
-    }, 325)
-
-    setTimeout(() => {
-      this.setState({
-        isArticleVisible: !this.state.isArticleVisible,
-        article: ''
-      })
-    }, 350)
-
-  }
-
-  handleClickOutside(event) {
-    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
-      if (this.state.isArticleVisible) {
-        this.handleCloseArticle();
-      }
-    }
-  }*/
 
   render() {
     return (
       <Layout location={this.props.location}>
-        <div className={`body ${this.state.loading} ${this.state.isArticleVisible ? 'is-article-visible' : ''}`}>
+        <div className={`body ${this.state.loading}`}>
           <div id="wrapper">
-            <Header timeout={this.state.timeout} />
-            <Footer timeout={this.state.timeout} />
+            <Header/>
+            <Footer/>
           </div>
           <div id="bg"></div>
         </div>
@@ -102,16 +37,5 @@ class IndexPage extends React.Component {
     )
   }
 }
-
-/* Removed popup article links*/
-// onOpenArticle={this.handleOpenArticle}
-/*<Main
-  isArticleVisible={this.state.isArticleVisible}
-  timeout={this.state.timeout}
-  articleTimeout={this.state.articleTimeout}
-  article={this.state.article}
-  onCloseArticle={this.handleCloseArticle}
-  setWrapperRef={this.setWrapperRef}
-/>*/
 
 export default IndexPage
